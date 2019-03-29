bcc0c7c4ca7b0ae176166b98826e1cd356bc4221
karenahv <karenahv@gmail.com>
2019-03-29 14:26:57 -0500

    saludame2

--- solution.rb
+++ solution.rb
@@ -1,13 +1,8 @@
 require 'sinatra'
 
-get '/' do
-  if params[:nombre] == nil || params[:nombre] == ""
+get '/:nombre' do
   <<-HTML
-  <h1>Hola desconocido!</h1>
+<h1>Hola #{params[:nombre].capitalize}!</h1>
   HTML
-  else
-  <<-HTML
-  <h1>Hola #{params[:nombre].capitalize}!</h1>
-  HTML
-  end
+
 end
